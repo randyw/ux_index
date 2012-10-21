516d9a07898cf0aef012737a325251e859ffa531
Randy Webb <randallmwebb@gmail.com>
2012-10-20 21:30:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uxindex::Application.config.secret_token = 'addaf390c05f2d9bd8b895cb2324355f11937100cf127caf9170d201f794b2a11c58288d826d7daaa747531c24c63cf088c475c67e701efbb576197cb363dd92'
